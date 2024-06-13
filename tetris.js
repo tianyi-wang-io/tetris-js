0593f4f15f849f3b063241295116b54989d7f582
Tianyi <wangkuai171@gmail.com>
2024-06-13 01:13:45 -0500

    draw background

--- tetris.js
+++ tetris.js
@@ -1,5 +1,9 @@
-/** @type {CanvasRenderingContext2D} */
+const squareSize = 40
+const whiteSpace = 2
 const canvas = document.getElementById('canvas')
+const gameWindow = canvas.getBoundingClientRect()
+const gameWindowWith = gameWindow.width;
+const gameWindowHeight = gameWindow.height;
 const ctx = canvas.getContext('2d')
 
 function drawRect(x, y, width, height, color) {
@@ -8,7 +12,18 @@ function drawRect(x, y, width, height, color) {
 }
 
 let drawBackground = () => {
-    drawRect(0, 0, 400, 800, '#9df5f5')
+    drawRect(0, 0, canvas.width, canvas.height, '#9df5f5');
+    for (let i = 0; i < gameWindowWith / 4; i++) {
+        for (let j = 0; j < gameWindowHeight / 4; j++) {
+            drawRect(
+                i * squareSize + whiteSpace,
+                j * squareSize + whiteSpace,
+                squareSize - whiteSpace,
+                squareSize - whiteSpace,
+                '#c3a8f7'
+            )
+        }
+    }
 }
 
 drawBackground()
