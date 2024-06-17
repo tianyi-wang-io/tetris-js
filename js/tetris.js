c6f201a7254d656bfe158b70c617854848b0718b
Tianyi <wangkuai171@gmail.com>
2024-06-17 02:31:28 -0500

    add grid

--- js/tetris.js
+++ js/tetris.js
@@ -6,24 +6,25 @@ const gameWindowWith = gameWindow.width;
 const gameWindowHeight = gameWindow.height;
 const ctx = canvas.getContext('2d')
 
-function drawRect(x, y, width, height, color) {
+let drawRect = (x, y, width, height, color) => {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, width, height);
 }
 
 let drawBackground = () => {
-    drawRect(0, 0, canvas.width, canvas.height, '#9df5f5');
+    drawRect(0, 0, canvas.width, canvas.height, '#ffffff');
     for (let i = 0; i < gameWindowWith / 4; i++) {
         for (let j = 0; j < gameWindowHeight / 4; j++) {
             drawRect(
                 i * squareSize + whiteSpace,
                 j * squareSize + whiteSpace,
-                squareSize - whiteSpace,
-                squareSize - whiteSpace,
-                '#c3a8f7'
+                squareSize - whiteSpace * 2,
+                squareSize - whiteSpace * 2,
+                '#e6d9ff'
             )
         }
     }
 }
 
-drawBackground()
+drawBackground();
+let grid = new Grid(20, 10);
